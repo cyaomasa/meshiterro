3bfa9f25cd2ef8b2d60b7405ae19abe1122f85f6
EC2 Default User <ec2-user@ip-172-31-6-194.ap-northeast-1.compute.internal>
2023-06-14 03:00:41 +0000

    [Finish]4-25

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -3,3 +3,12 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
+
+const webpack = require('webpack')
+environment.plugins.prepend(
+    'Provide',
+    new webpack.ProvidePligin({
+        $: 'jquery/src/jquery',
+        Propper: 'popper.js'
+    })
+    )
